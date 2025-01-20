ee2abedb90f765005e49ca144b645ea6830a4147
ZhefengJin <wugis1219@gmail.com>
2025-01-20 16:44:18 +0900

    start

---
+++ src/pages/Main.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-card>
+        <v-toolbar color="cyan" dark flat>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+            <v-toolbar-title>Your Dashboard</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+        </v-toolbar>
+
+        <v-tabs v-model="tab" vertical>
+            <v-tab text="Leaflet Map" value="leafletMap">
+                <v-icon left> mdi-account </v-icon>
+                Leaflet Map
+            </v-tab>
+
+            <v-tab value="table">
+                <v-icon left> mdi-lock </v-icon>
+                Table
+            </v-tab>
+
+            <v-tab>
+                <v-icon left> mdi-lock </v-icon>
+                Option 2
+            </v-tab>
+        </v-tabs>
+
+        <v-tabs-window v-model="tab">
+            <v-tabs-window-item value="leafletMap">
+                <LeafletMap />
+            </v-tabs-window-item>
+            <v-tabs-window-item value="table">
+                <TestTable />
+            </v-tabs-window-item>
+        </v-tabs-window>
+    </v-card>
+</template>
+
+<script lang="ts">
+export default {
+    data: () => ({
+        tab: "leafletMap"
+    }),
+    methods: {}
+};
+</script>
---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-empty-state
+        action-text="Retry Request"
+        image="https://cdn.vuetifyjs.com/docs/images/components/v-empty-state/connection.svg"
+        text="There might be a problem with your connection or our servers. Please check your internet connection or try again later. We appreciate your patience."
+        title="Something Went Wrong"
+        @click:action="onClickAction"
+    ></v-empty-state>
+</template>
+
+<script lang="ts">
+export default {
+    methods: {
+        onClickAction() {
+            alert("You clicked the action button");
+        }
+    }
+};
+</script>
